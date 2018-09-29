cdb5d8b1c1f98ff6a0d6c2316fb248c71aea3b4c
DarkDream99 <denys.zaluzhnyi@gmail.com>
2018-09-29 15:30:38 +0300

    add interface for cryptodes

---
+++ des/cryptodes/des.py
@@ -0,0 +1,15 @@
+def encrypt(data_bytes: bytearray, key_bytes: bytearray) -> bytearray:
+
+    raise NotImplementedError()
+
+
+def decrypt(code_bytes: bytearray, key_bytes: bytearray) -> bytearray:
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    pass
+    # text = "hello "
+    #
+    # bits = bytearray(text, encoding="utf-8")
+    # print(bits)
