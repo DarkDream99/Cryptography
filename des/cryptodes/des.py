815d2a0d22004fb5b5bec9cf62998b2c17da96c8
DarkDream99 <denys.zaluzhnyi@gmail.com>
2018-09-29 16:16:42 +0300

    add process of initial permutation for cryptodes

--- des/cryptodes/des.py
+++ des/cryptodes/des.py
@@ -1,6 +1,18 @@
+from tables import initial_permutation
+
+
 def encrypt(data_bytes: bytearray, key_bytes: bytearray) -> bytearray:
-    
-    raise NotImplementedError()
+    while len(data_bytes) < 64:
+        data_bytes.append(0)
+
+    print(data_bytes.decode("utf-8"))
+    for ind in range(1, len(data_bytes) + 1):
+        data_bytes[ind - 1], data_bytes[initial_permutation[ind] - 1] = \
+            data_bytes[initial_permutation[ind] - 1], data_bytes[ind - 1]
+
+    print(data_bytes.decode("utf-8"))
+
+    # raise NotImplementedError()
 
 
 def decrypt(code_bytes: bytearray, key_bytes: bytearray) -> bytearray:
@@ -8,7 +20,7 @@ def decrypt(code_bytes: bytearray, key_bytes: bytearray) -> bytearray:
 
 
 if __name__ == "__main__":
-    pass
+    encrypt(bytearray("Hello Denys. I'm fine", encoding="utf-8"), bytearray("key", encoding="utf-8"))
     # text = "hello "
     #
     # bits = bytearray(text, encoding="utf-8")
