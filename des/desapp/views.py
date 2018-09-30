b036226f08f3499b5ed7228a8349b774bd6d7db3
DarkDream99 <denys.zaluzhnyi@gmail.com>
2018-09-30 23:54:23 +0300

    add page index

--- des/desapp/views.py
+++ des/desapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Main page dor des algorithm!")
+    template = loader.get_template('desapp/index.html')
+    context = {}
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'desapp/index.html', context)
