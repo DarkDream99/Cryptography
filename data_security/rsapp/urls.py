da13fb7ceee5764b71d9e2d3458f67338776e99b
DarkDream99 <denys.zaluzhnyi@gmail.com>
2018-10-14 16:36:27 +0300

    add html for swapping keys

--- data_security/rsapp/urls.py
+++ data_security/rsapp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='RSA'),
     re_path('create_keys(\.html)?/', views.create_keys),
+    re_path('swap_keys(\.html)?/', views.swap_keys),
 ]
--- data_security/rsapp/views.py
+++ data_security/rsapp/views.py
@@ -9,3 +9,8 @@ def index(request):
 def create_keys(request, *argv):
     context = {}
     return render(request, 'rsapp/create_keys.html', context)
+
+
+def swap_keys(request, client_key=None, *args):
+    context = {}
+    return render(request, 'rsapp/swap_keys.html', context)
