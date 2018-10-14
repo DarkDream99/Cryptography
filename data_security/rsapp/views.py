8d5dc9f078a1b271f5c11f5f7b59f387f4a49b37
DarkDream99 <denys.zaluzhnyi@gmail.com>
2018-10-14 19:53:13 +0300

    add files to save objects

--- data_security/rsapp/views.py
+++ data_security/rsapp/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .libs.cryptorsa import cryptorsa
+import os
+import pickle
+
+
+VIEW_POSITION = os.path.dirname(os.path.realpath(__file__))
+KEYS = set()
+CRYPT_TEXT = b""
 
 
 def index(request):
@@ -7,11 +16,77 @@ def index(request):
 
 
 def create_keys(request, *args):
-    context = {}
-    return render(request, 'rsapp/create_keys.html', context)
+    path_to_pub = os.path.join(VIEW_POSITION, 'files', 'obj', 'pub_client.key')
+    path_to_priv = os.path.join(VIEW_POSITION, 'files', 'obj', 'priv_client.key')
+
+    if 'priv_client_key' not in KEYS:
+        pub_key, priv_key = cryptorsa.create_keys()
+
+        hpub_client = open(path_to_pub, 'wb')
+        pickle.dump(pub_key, hpub_client)
+
+        hpriv_client = open(path_to_priv, 'wb')
+        pickle.dump(priv_key, hpriv_client)
+
+        request.session['pub_client_key'] = path_to_pub
+        request.session['priv_client_key'] = path_to_priv
+
+        KEYS.add('priv_client_key')
+        KEYS.add('pub_client_key')
+
+        hpub_client.close()
+        hpriv_client.close()
+
+        context = {
+            'public': pub_key,
+            'private': priv_key
+        }
+
+        return render(request, 'rsapp/create_keys.html', context)
+    else:
+        hpub_client = open(request.session['pub_client_key'], 'rb')
+        pub_client = pickle.load(hpub_client)
+        hpriv_client = open(request.session['priv_client_key'], 'rb')
+        priv_client = pickle.load(hpriv_client)
+
+        context = {
+            'public': pub_client,
+            'private': priv_client
+        }
+        return render(request, 'rsapp/create_keys.html', context)
 
 
 def swap_keys(request, client_key=None, *args):
+    path_to_pub_server = os.path.join(VIEW_POSITION, 'files', 'obj', 'pub_server.key')
+    path_to_priv_server = os.path.join(VIEW_POSITION, 'files', 'obj', 'priv_server.key')
+
+    if 'pub_server_key' not in KEYS:
+        pub_key, priv_key = cryptorsa.create_keys()
+
+        hpub_server = open(path_to_pub_server, 'wb')
+        hpriv_server = open(path_to_priv_server, 'wb')
+
+        pickle.dump(pub_key, hpub_server)
+        pickle.dump(priv_key, hpriv_server)
+
+        request.session['pub_server_key'] = path_to_pub_server
+        request.session['priv_server_key'] = path_to_priv_server
+
+        hpub_server.close()
+        hpriv_server.close()
+
+    if 'pub_client_key' in KEYS:
+        hpub_client = open(request.session['pub_client_key'], 'rb')
+        pub_client = pickle.load(hpub_client)
+        hpub_server = open(request.session['pub_server_key'], 'rb')
+        pub_server = pickle.load(hpub_server)
+
+        context = {
+            'pub_client_key': pub_client,
+            'pub_server_key': pub_server
+        }
+        return render(request, 'rsapp/swap_keys.html', context)
+
     context = {}
     return render(request, 'rsapp/swap_keys.html', context)
 
@@ -22,10 +97,24 @@ def change_text(request, bits=None, *args):
 
 
 def crypt(request, message=None, *args):
-    context = {}
+    if not message:
+        context = {}
+        return render(request, 'rsapp/crypt_text.html', context)
+
+    path_to_pub_server = os.path.join(VIEW_POSITION, 'files', 'obj', 'pub_server.key')
+    hpub_client = open(path_to_pub_server, 'rb')
+    pub_client = pickle.load(hpub_client)
+
+    global CRYPT_TEXT
+    CRYPT_TEXT = cryptorsa.crypt(pub_client, message)
+
+    context = {
+        'res_bytes': CRYPT_TEXT
+    }
+
     return render(request, 'rsapp/crypt_text.html', context)
 
 
 def decrypt(request, bits=None, *args):
-    context={}
+    context = {}
     return render(request, 'rsapp/decrypt_bits.html', context)
